cd072ba89398103d686b7e9c869e98d0c319c885
Monica <monicayanetha@gmail.com>
2022-10-19 10:15:58 -0500

    firs commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+#Hola Mundo
+print("Hola Python")
+
+"""
+Este es un comentario
+en varias lineas
+"""
+
+'''
+Otra forma de hacer un 
+comentario
+'''
+
+#Consultar el tipo de dato
+print(type(('Hola mundo')))
+print(type(5))
+print(type(5.3))
